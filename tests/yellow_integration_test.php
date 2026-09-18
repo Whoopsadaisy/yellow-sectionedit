fc6f12bbff29ae483cc7e7062f12f3837f40cf10
Whoopsadaisy <99804696+Whoopsadaisy@users.noreply.github.com>
2026-09-18 11:51:00 +0200

    Run Yellow integration test from Yellow root

--- tests/yellow_integration_test.php
+++ tests/yellow_integration_test.php
@@ -22,6 +22,8 @@ file_put_contents("$root/content/1-home/page.md", "# Home\n");
 file_put_contents("$root/content/2-wiki/page.md", "## Pages\n\nSome text\n");
 file_put_contents("$root/content/2-wiki/juniper-vpn.md", "## Juniper VPN\n");
 
+$cwd = getcwd();
+chdir($root);
 $yellow = new YellowCore();
 $yellow->load();
 $edit = $yellow->extension->get("edit");
